a803de55b62cb6a421129717c526c227d6610743
stfDeveloper <sstefancosmin@icloud.com>
2022-01-16 22:13:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let app = new Vue ({
+    el:"#root",
+    data:{
+        immagini : [
+            Svezia = {
+                immagine: "img/01.jpg",
+                titolo: "Svezia",
+                testo: "lorem ipsum dolor"
+            },
+            Svizzera = {
+                immagine: "img/02.jpg",
+                titolo: "Svizzera",
+                testo: "lorem ipsum dolor"
+            },
+            GranBretagna = {
+                immagine: "img/03.jpg",
+                titolo: "Gran Bretagna",
+                testo: "lorem ipsum dolor"
+            },
+            Germania = {
+                immagine: "img/04.jpg",
+                titolo: "Germania",
+                testo: "lorem ipsum dolor"
+            },
+            Paradise = {
+                immagine: "img/05.jpg",
+                titolo: "Svezia",
+                testo: "lorem ipsum dolor"
+            },
+        ]
+    }
+})
